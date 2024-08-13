51b5a8001a94601eef268e5ef0b908185a19dbc3
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-08-13 18:56:10 +0530

    hello

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -4,7 +4,7 @@ const templates = [
         name:"Blog Title",
         desc:"An AI tool thst generate blog title depend on your blog information",
         category:"Blog",
-        icon:'https://www.flaticon.com/free-icon-font/youtube_6422215?page=1&position=1&term=youtube&origin=search&related_id=6422215',
+        icon:'https://cdn-icons-png.flaticon.com/128/6422/6422215.png',
         aiPrompt:'Give me 5 blog topicidea in bullet wise only based on give niche topic and give me result in Rich text editor format',
         slug:'generate-blog-title', 
         form:[
@@ -25,7 +25,7 @@ const templates = [
         name:"Youtube Title",
         desc:"An AI tool that will produce youtube title",
         category:"Title",
-        icon:'BlogIcon',
+        icon:'https://cdn-icons-png.flaticon.com/128/9291/9291832.png',
         aiPrompt:'Give me 5 Tiltle topic idea in bullet wise only based on give niche topic and give me result in Rich text editor format',
         slug:'generate-blog-title', 
         form:[
