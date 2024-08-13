68c9fa5e70b8e0537f685540005c01e403af8d2f
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-08-13 17:11:20 +0530

    hello

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -1,20 +1,3 @@
-// /export default [
-//     {
-//         name:"Blog Title",
-//         desc:"An AI tool thst generate blog title depend on your blog information",
-//         category:"Blog",
-//         icon:'BlogIcon',
-//         aiPrompt:'Give me 5 blog topicidea in bullet wise only based on give niche topic and give me result in Rich text editor format',
-//         slug:'generate-blog-title', 
-//         form:[
-//             {
-//                 label:"Enter your blog niche",
-//                 field:'input',
-//                 name:'niche',
-//                 required:true
-//             }
-//         ]
-//     }
 
 const templates = [
     {
@@ -29,6 +12,32 @@ const templates = [
                 label:"Enter your blog niche",
                 field:'input',
                 name:'niche',
+                required:true
+            }, {
+                label:"Enter youtube video Outline here(optional)",
+                field:'textarea',
+                name:'Outline',
+                required:true
+            }
+        ]
+    },
+    {
+        name:"Blog Title",
+        desc:"An AI tool thst generate blog title depend on your blog information",
+        category:"Blog",
+        icon:'BlogIcon',
+        aiPrompt:'Give me 5 blog topicidea in bullet wise only based on give niche topic and give me result in Rich text editor format',
+        slug:'generate-blog-title', 
+        form:[
+            {
+                label:"Enter your blog niche",
+                field:'input',
+                name:'niche',
+                required:true
+            }, {
+                label:"Enter youtube video Outline here(optional)",
+                field:'textarea',
+                name:'Outline',
                 required:true
             }
         ]
