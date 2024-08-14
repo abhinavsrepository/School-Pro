16e428cdeba6fa9922993139dc2ba71a64527189
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-08-14 16:30:23 +0530

    hello

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -22,11 +22,20 @@ function TemplateListSection({userSearchInput}:any) {
 
   const[templateList,setTemplateList]=useState(Templates)
   useEffect (()=>{
-    console.log(userSearchInput),[userSearchInput]
-  })
+    
+    if(userSearchInput)
+    {
+      const filterData =Templates.filter(item => item.name.toLowerCase().includes(userSearchInput.toLowerCase()))
+      ;
+      setTemplateList(filterData)
+    }
+    else {
+      setTemplateList(Templates)
+    }
+  },[userSearchInput])
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10 '>
-      {Templates.map((item:TEMPLATE,index:number)=>(
+      {templateList.map((item:TEMPLATE,index:number)=>(
        
        <TemplateCard key={index}{...item}/>
        ))}
