0c762fa54e253752cb1ef83818983e6a9828ab96
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-08-16 16:44:37 +0530

    ello

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import FormSection from './_components/FormSection'
-import OutputSection from './_components/OutputSection'
+"use client"
+import React, { useState } from 'react'
+import FormSection from '../_components/FormSection'
+import OutputSection from '../_components/OutputSection'
 import {TEMPLATE} from '../../_components/TemplateListSection'
 import Templates from '@/app/(data)/Templates'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, Link } from 'lucide-react'
 interface PROPS{
     params:{
         'template-slug':string
@@ -10,14 +13,25 @@ interface PROPS{
 }
 
 function CreateNewContent (props:PROPS) {
-   const selectedTemplate:TEMPLATE|undefined= Templates?.find((item)=>item.slug==props.params['template-slug'])
+   const selectedTemplate:TEMPLATE|undefined= Templates?.find((item)=>item.slug==props.params['template-slug']);
+   const [loading,setLoading]=useState(false);
+   const  GenerateAIContent =(formData:any)=>{
+
+   }
   return (
+    <div>
+      <Link href={"/dashboard"}>
+      <Button ><ArrowLeft/>Back</Button>
+      </Link>
     <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5 '>
       {/* FormSection */}
-      <FormSection selectedTemplate={selectedTemplate}/>
+      <FormSection selectedTemplate={selectedTemplate}
+      userFormInput={(v:any )=>console.log(v)}
+      loading={loading}/>
     {/* outsection */}
     <div className='col-span-3'>
     <OutputSection/>
+    </div>
     </div>
     </div>
   )
